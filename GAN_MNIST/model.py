e6fcfbb56dd789f75ec0d7cebf3f86ff5b7c6f8d
dovietchinh <dovietchinh1998@gmail.com>
2022-04-27 21:14:58 +0700

    model.py

--- GAN_MNIST/model.py
+++ GAN_MNIST/model.py
@@ -1,5 +1,5 @@
 import torch
-
+import torch.nn.functional as F
 class Discriminator(torch.nn.Module):
 
     def __init__(self):
@@ -28,23 +28,58 @@ class Generator(torch.nn.Module):
         super(Generator,self).__init__()
         self.Sequence = torch.nn.ModuleList()
         self.size = 28,28
-        self.Sequence.append(torch.nn.Linear(laten_dim,128*7*7))
+        self.Sequence.append(torch.nn.Linear(laten_dim,64*7*7))
         self.Sequence.append(torch.nn.ReLU())
         # self.Sequence.append(torch.nn.Reshape((128,7,7)))
-        self.Sequence.append(torch.nn.ConvTranspose2d(128,128,(4,4),stride=2,padding=1))
+        self.Sequence.append(torch.nn.ConvTranspose2d(64,64,(4,4),stride=2,padding=1))
         self.Sequence.append(torch.nn.ReLU())
-        self.Sequence.append(torch.nn.ConvTranspose2d(128,128,(4,4),stride=2,padding=1))
+        self.Sequence.append(torch.nn.ConvTranspose2d(64,64,(4,4),stride=2,padding=1))
         self.Sequence.append(torch.nn.ReLU())
-        self.Sequence.append(torch.nn.Conv2d(128,1,(7,7),stride=1,padding=3))
-        self.Sequence.append(torch.nn.Sigmoid())
+        self.Sequence.append(torch.nn.Conv2d(64,1,(7,7),stride=1,padding=3))
+        # self.Sequence.append(torch.nn.Sigmoid())
 
     def forward(self,x):
         for index,module in enumerate(self.Sequence):
             if index == 2:
-                x = torch.reshape(x,(-1,128,7,7))
+                x = torch.reshape(x,(-1,64,7,7))
             x = module(x)
             # print(x.shape)
         return x
+
+
+class Discriminator(torch.nn.Module):
+    def __init__(self, ):
+        super(Discriminator, self).__init__()
+        self.net1 = torch.nn.Linear(28*28,500)
+        self.net2 = torch.nn.Linear(500,250)
+        self.net3 = torch.nn.Linear(250,1)
+        self.sm = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = torch.flatten(x,start_dim=1)
+        x =  self.net1(x)
+        x =  self.net2(x)
+        x =  self.net3(x)
+        x = self.sm(x)
+        return x
+
+class Generator(torch.nn.Module):
+    def __init__(self, ):
+        super(Generator, self).__init__()
+        self.net1 = torch.nn.Linear(100,28*28)
+        # self.net3 = torch.nn.Linear(500,28*28)
+        self.sm = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x =  self.net1(x)
+        # x =  self.net2(x)
+        # x =  self.net3(x)
+        x = self.sm(x)
+        x = x.reshape(-1,1,28,28)
+        return x
+# D = SimpleNN(batch_size, D_img, D_hidden, 1)
+# G = SimpleNN(batch_size, D_ent, D_hidden, D_img)
+
 
 
 class GAN(torch.nn.Module):
@@ -64,12 +99,14 @@ class GAN(torch.nn.Module):
 if __name__ =='__main__':
     x = torch.rand(10,1,28,28)
     # g_model = 
-    d_model = Discriminator();
+    
     # # print(model(x))
     
-    # x = torch.rand(2,100)
+    z = torch.rand(2,100)
     g_model = Generator();
-    d_model(x)
+    d_model = Discriminator();
+    print(d_model(x).shape)
+    print(g_model(z).shape)
     # gan_model = GAN(d_model,g_model)
     # print(gan_model.d_model is d_model)
     # print(gan_model.g_model is g_model)
--- GAN_MNIST/training.py
+++ GAN_MNIST/training.py
@@ -32,7 +32,7 @@ def main():
     g_model = model.Generator().to(device)
     gan_model = model.GAN(d_model,g_model).to(device)
     opt1 = torch.optim.Adam(d_model.parameters())
-    opt2 = torch.optim.Adam(g_model.parameters())
+    opt2 = torch.optim.Adam(gan_model.g_model.parameters())
     criterior1 = torch.nn.BCELoss()
     criterior2 = torch.nn.BCELoss()
     for epoch in tqdm(range(1000),total=1000,desc='GAN',leave=True):
@@ -44,19 +44,32 @@ def main():
 
             d_model.train()
             gan_model.d_model.train()
+            g_model.eval()
+            gan_model.g_model.eval()
+            gan_model.d_model.requires_grad = True
+            gan_model.g_model.requires_grad = False
+            d_model.requires_grad = True
+            g_model.requires_gra = False
+
             label_real = torch.ones(img_real.shape[0],1).to(device)
             img_real = img_real.to(device)
             # X, y = vstack((X_real, X_fake)), vstack((y_real, y_fake))
-            img_fake = torch.rand(img_real.shape[0],1,28,28).to(device)
+            #img_fake = torch.rand(img_real.shape[0],1,28,28).to(device)
+            img_fake = g_model(torch.rand(img_real.shape[0],laten_dim).to(device))
+            #print(img_fake.shape)
+            #print(img_real.shape)
+            #exit()
             label_fake = torch.zeros(img_real.shape[0],1).to(device)
             X = torch.cat([img_real,img_fake],dim=0)
             Y = torch.cat([label_real,label_fake],dim=0)
             Y_pred = d_model(X)
-            opt1.zero_grad()
+            
+            # [o.zeros_grad() for o in opt1.values()]
             # print(Y)
             # print(Y_pred)
             
             d_loss = criterior1(Y_pred,Y)
+            opt1.zero_grad()
             d_loss.backward()
             # print(d_loss)
             # exit()
@@ -68,12 +81,19 @@ def main():
             g_model.train()
             gan_model.d_model.eval()
             gan_model.g_model.train()
-            x_laten = torch.rand(img_real.shape[0],laten_dim).to(device)
-            y_laten = torch.ones(img_real.shape[0],1).to(device)
+            gan_model.d_model.requires_grad = False
+            gan_model.g_model.requires_grad = True
+            d_model.requires_grad = False
+            g_model.requires_grad = True
+            x_laten = torch.rand(img_real.shape[0]*2,laten_dim).to(device)
+            y_laten = torch.ones(img_real.shape[0]*2,1).to(device)
 
-            opt2.zero_grad()
+            
+            # [o.zeros_grad() for o in opt2.values()]
             gan_pred = gan_model(x_laten)
+            
             gan_loss = criterior2(gan_pred,y_laten)
+            opt2.zero_grad()
             gan_loss.backward()
             # print(gan_loss)
             opt2.step()
@@ -95,6 +115,7 @@ def main():
             col = index // 10
             image_view[row*28:row*28+28,col*28:col*28+28,:] = view[index]
         cv2.imshow('view',image_view.astype('uint8'))
+        cv2.imwrite(f'saved_img/model_{epoch}.jpg',image_view.astype('uint8'))
         cv2.waitKey(1)
         torch.save({'g_model' : g_model.state_dict(),
                     'd_model' : d_model.state_dict()},
